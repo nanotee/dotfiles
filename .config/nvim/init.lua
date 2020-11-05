3862aeb93aead23472f9abde40ac4a491d632334
Timothée Sterle <timothee-s@mailoo.org>
2020-11-05 20:24:10 +0100

    move stuff out of init.vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,9 @@
+vim.g.polyglot_disabled = {'sensible'}
+vim.g.javascript_plugin_jsdoc = 1
+vim.cmd [[packadd! vim-polyglot]]
+vim.cmd [[runtime! ftdetect/polyglot.vim]]
+require'profiler'
+require'my.utils.debug'
 require'my.utils'
 require'my.statusline'
 reload'my.config.options'
--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,7 +1,2 @@
 let $MYVIMRC = stdpath('config') .. '/init.lua'
-packadd! profiler.nvim
-lua require('profiler')
 luafile $MYVIMRC
-packadd! vim-polyglot
-let g:polyglot_disabled = ['sensible']
-runtime! ftdetect/polyglot.vim

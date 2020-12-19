90681903cb4796cce2bceb8b8b7701c54d9acb04
Timothée Sterle <timothee-s@mailoo.org>
2020-12-19 22:12:49 +0100

    update snippets

--- .config/nvim/lua/my/config/plugins/snippets-nvim.lua
+++ .config/nvim/lua/my/config/plugins/snippets-nvim.lua
@@ -6,10 +6,6 @@ map.i['<C-A-k>'] = {"<Cmd>lua require'snippets'.expand_or_advance()<CR>", 'norem
 map.i['<C-A-j>'] = {"<Cmd>lua require'snippets'.expand_or_advance(-1)<CR>", 'noremap'}
 
 require'snippets'.snippets = {
-    _global = {
-        date = '${=os.date("%Y-%m-%d")}',
-        datel = '${=os.date("%Y-%m-%dT%H:%M:%S+01:00")}',
-    },
     lua = {
         ['for'] = idt'for ${1:k}, ${2:v} in ipairs($3) do\n$0\nend',
         ['if'] = idt'if $1 then\n$0\nend',
@@ -18,7 +14,9 @@ require'snippets'.snippets = {
         ["local"] = 'local ${2:${1|S.v:match"[^.]+$"}} = ${1}',
     },
     markdown = {
-        cb = '```$1\n$0\n```',
+        date = '${=os.date("%Y-%m-%d")}',
+        datel = '${=os.date("%Y-%m-%dT%H:%M:%S+01:00")}',
+        code = '```$1\n$0\n```',
     },
     c = {
         boilerplate =

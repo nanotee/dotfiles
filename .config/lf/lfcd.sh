ac4ba00bf0320b6e08f8678d1d9da8ddec4cb14a
Timothée Sterle <timothee-s@mailoo.org>
2021-01-17 16:22:00 +0100

    make lfcd a shell function again
    Invoking cd in a subshell is obviously not what I want.

---
+++ .config/lf/lfcd.sh
@@ -0,0 +1,15 @@
+# Change working dir in shell to last dir in lf on exit (adapted from ranger).
+
+lfcd() {
+    tmp="$(mktemp)"
+    lf -last-dir-path="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp"
+        if [ -d "$dir" ]; then
+            if [ "$dir" != "$(pwd)" ]; then
+                cd "$dir" || exit
+            fi
+        fi
+    fi
+}

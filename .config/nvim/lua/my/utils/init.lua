57a15c1ecef0cc9718b4e4188c0e5f40b776d376
Timothée Sterle <timothee-s@mailoo.org>
2021-01-30 23:11:21 +0100

    refactor command runner

--- .config/nvim/lua/my/utils/init.lua
+++ .config/nvim/lua/my/utils/init.lua
@@ -82,23 +82,31 @@ function M.live_server_stop(job_id)
     vim.cmd 'delcommand LiveServerStop'
 end
 
+local filetype_cmds = {
+    javascript = {'node'},
+    typescript = {'env', 'NO_COLOR=1', 'deno', 'run', '--quiet', '--allow-read', '-'},
+    r = {'R', '--vanilla', '--quiet'},
+    lua = function(lines)
+        assert(loadstring(table.concat(lines, '\n')))()
+    end,
+    vim = function(lines)
+        print(fn.execute(lines))
+    end,
+    c = function()
+        print(fn.system({'c', fn.expand('%')}))
+    end,
+}
+
 function M.run(line1, line2, cmd)
     local lines = fn.getline(line1, line2)
     if cmd == '' then cmd = vim.bo.filetype end
 
-    if cmd == 'vim' then
-        print(fn.execute(lines))
-    elseif cmd == 'lua' then
-        assert(loadstring(table.concat(lines, '\n')))()
-    else
-        local filetype_cmds = {
-            javascript = {'node'},
-            typescript = {'env', 'NO_COLOR=1', 'deno', 'run', '--quiet', '--allow-read', '-'},
-            r = {'R', '--vanilla', '--quiet'},
-        }
-        local stdout = fn.system(filetype_cmds[cmd] or cmd, lines)
-        print(stdout)
+    if type(filetype_cmds[cmd]) == 'function' then
+        filetype_cmds[cmd](lines)
+        return
     end
+    local stdout = fn.system(filetype_cmds[cmd] or cmd, lines)
+    print(stdout)
 end
 
 return M

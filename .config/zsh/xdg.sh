21a3d17fc0f5cd210041efac32ccbe19e13f2122
Timothée Sterle <timothee-s@mailoo.org>
2021-02-21 18:56:58 +0100

    update env vars

--- .config/zsh/xdg.sh
+++ .config/zsh/xdg.sh
@@ -2,6 +2,7 @@
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_BIN_HOME="$HOME/.local/bin"
 export XDG_CACHE_HOME="$HOME/.cache"
 
 # Make as many applications XDG compliant as possible

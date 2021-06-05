386f6ee1b0a0246ed265f86778681539ef519972
Timothée Sterle <timothee-s@mailoo.org>
2021-06-05 14:52:24 +0200

    switch to vim.opt

--- .config/nvim/lua/my/config/options.lua
+++ .config/nvim/lua/my/config/options.lua
@@ -1,36 +1,45 @@
+local g, opt = vim.g, vim.opt
 -- Appearence
-vim.g.colors_name = 'dracula'
-vim.o.termguicolors = true
-vim.wo.number = true
-vim.wo.signcolumn = 'number'
-vim.wo.list = true
-local listchars = {
-     'tab:│ ',
-     'eol:¬',
-     'nbsp:␣',
-     'trail:•',
+g.colors_name = 'dracula'
+opt.termguicolors = true
+opt.number = true
+opt.signcolumn = 'number'
+opt.list = true
+opt.listchars = {
+     tab = '│ ',
+     eol = '¬',
+     nbsp = '␣',
+     trail = '•',
 }
-vim.o.listchars = table.concat(listchars, ',')
-local fillchars = {
-    'eob: ',
-    'stlnc:─',
-    'diff:·',
+opt.fillchars = {
+    eob = ' ',
+    stlnc = '─',
+    diff = '·',
 }
-vim.o.fillchars = table.concat(fillchars, ',')
-vim.wo.linebreak = true
-vim.wo.breakindent = true
-vim.o.showmode = false
-vim.o.pumblend = 10
-vim.o.pumheight = 25
-vim.o.title = true
-vim.o.guifont = 'mononoki Nerd Font'
+opt.linebreak = true
+opt.breakindent = true
+opt.showmode = false
+opt.pumblend = 10
+opt.pumheight = 25
+opt.title = true
+opt.guifont = 'mononoki Nerd Font'
 
 -- Cursor
-vim.wo.cursorline = true
-vim.o.mouse = 'a'
-vim.o.mousetime = 0
-vim.o.whichwrap = 'b,h,l,s,<,>,[,],~'
-local guicursor = {
+opt.cursorline = true
+opt.mouse = 'a'
+opt.mousetime = 0
+opt.whichwrap = {
+    ['b'] = true,
+    ['h'] = true,
+    ['l'] = true,
+    ['s'] = true,
+    ['<'] = true,
+    ['>'] = true,
+    ['['] = true,
+    [']'] = true,
+    ['~'] = true,
+}
+opt.guicursor = {
     'n-v-c:block',
     'i-ci-ve:ver25',
     'r-cr:hor20',
@@ -38,60 +47,50 @@ local guicursor = {
     'a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor',
     'sm:block-blinkwait175-blinkoff150-blinkon175',
 }
-vim.o.guicursor = table.concat(guicursor, ',')
-vim.o.virtualedit = 'block'
+opt.virtualedit = 'block'
 
 -- Indentation behaviour
-vim.bo.expandtab = true
-vim.o.expandtab = true
-vim.bo.shiftwidth = 4
-vim.o.shiftwidth = 4
-vim.bo.tabstop = 4
-vim.o.tabstop = 4
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
 
 -- Search Behaviour
-vim.o.ignorecase = true
-vim.o.inccommand = 'split'
+opt.ignorecase = true
+opt.inccommand = 'split'
 
 -- Splits
-vim.o.splitright = true
-vim.o.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
 -- External programs
-vim.o.grepprg = 'rg --vimgrep --no-heading --smart-case'
-vim.o.formatprg = 'prettier --stdin-filepath=%'
+opt.grepprg = 'rg --vimgrep --no-heading --smart-case'
+opt.formatprg = 'prettier --stdin-filepath=%'
 
 -- Deactivate plugins I don't use
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_tutor_mode_plugin = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_gzip = 1
-vim.g.loaded_2html_plugin = 1
-vim.g.did_install_default_menus = 1
-vim.g.did_install_syntax_menu = 1
+g.loaded_netrwPlugin = 1
+g.loaded_tutor_mode_plugin = 1
+g.loaded_tarPlugin = 1
+g.loaded_zipPlugin = 1
+g.loaded_gzip = 1
+g.loaded_2html_plugin = 1
+g.did_install_default_menus = 1
+g.did_install_syntax_menu = 1
 
 -- Providers
-vim.g.loaded_python_provider = 0
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_ruby_provider = 0
+g.loaded_python_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_node_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
 
 -- Miscellaneous
-vim.o.lazyredraw = true
-vim.o.updatetime = 300
-vim.bo.spelllang = 'fr'
-vim.o.spelllang = 'fr'
-vim.o.hidden = true
-vim.bo.undofile = true
-vim.o.undofile = true
-vim.o.switchbuf = 'usetab'
-vim.o.shortmess = vim.o.shortmess .. 'cI'
-local completeopt = {
-    'menuone',
-    'noselect',
-}
-vim.o.completeopt = table.concat(completeopt, ',')
-vim.o.joinspaces = false
-vim.o.shada = "!,'100,<50,s10,h,:1000,/1000"
+opt.lazyredraw = true
+opt.updatetime = 300
+opt.spelllang = 'fr'
+opt.hidden = true
+opt.undofile = true
+opt.switchbuf = 'usetab'
+opt.shortmess:append{c = true, I = true}
+opt.completeopt = {'menuone', 'noselect'}
+opt.joinspaces = false
+opt.shada = {'!', "'100", '<50', 's10', 'h', ':1000', '/1000'}
